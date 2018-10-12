316a65d90dec60ca99b9265e022e7c26b1d1d51b
SharleenMock <sharleenmock@gmail.com>
2018-10-11 23:18:30 -0700

    Combining Words

--- my_script.js
+++ my_script.js
@@ -10,4 +10,17 @@ function countDown() {
 	}
 
 	document.getElementById("countDownButton").innerHTML = newVal;
+
+  if(currentVal == 1){
+    document.getElementById("trial").style.display="block";
+  }
+
+
+}
+
+function show_name(){
+  last_name = document.getElementById("last").value;
+  first_name = document.getElementById("first").value;
+  full = last_name + first_name;
+  document.getElementById("full_name").innerHTML = full;
 }
